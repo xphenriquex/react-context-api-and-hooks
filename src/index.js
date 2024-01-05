b319b011a28356235d47055f65126b10b597f860
Paulo Henrique <p.henriquerm@hotmail.com>
2024-01-05 16:08:37 -0300

    Context API + Hooks (unindo as ideias) - Parte 1

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global-styles.css';
 import Home from './templates/Home';
+import { CounterContextProvider } from './contexts/CounterContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <CounterContextProvider>
+      <Home />
+    </CounterContextProvider>
   </React.StrictMode>
 );
 
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,10 @@
+import { useCounterContext } from '../../contexts/CounterContext';
 import './styles.css';
 
+
 export const Home = () => {
-    
+  const [state, dispatch] = useCounterContext();
+  
   return (
     <div>
       <h1>Oi</h1>
